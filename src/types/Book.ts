03c0ef6760500a527cfb84e99ce1846da996f11e
Vlad Solovey <vlad.solovey7@gmail.com>
2025-06-15 09:41:34 +0300

    Added types to Book, BookDescriptionFull

---
+++ src/types/Book.ts
@@ -0,0 +1,16 @@
+export interface BookPreview{
+    id: number,
+    title: string,
+    author: string,
+    image: string
+}
+
+export interface BookDescription {
+    author: string,
+    description: string,
+    id: number,
+    image: string,
+    pages: number,
+    title: string,
+    yearOfPublish: string
+}
