61546f00d6852e2376d2aeba9cd131821485e6b6
Vlad Solovey <vlad.solovey7@gmail.com>
2025-06-16 09:26:57 +0300

    Added types to SearchBookToUpdateView

--- views/SearchBookToUpdateView.vue
+++ views/SearchBookToUpdateView.vue
@@ -1,14 +1,14 @@
-<script setup>
+<script setup lang="ts">
 
     import { useRouter } from 'vue-router';
     import { ref } from 'vue';
     import { useModeStore } from '../stores/editModeStore';
 
-    const searchValue = ref(null)
     const router = useRouter()
     const modeStore = useModeStore()
+    const searchValue = ref<string>('')
 
-    function navigateToEdit() {
+    function navigateToEdit(): void {
     if (!searchValue.value?.trim()) {
         alert('Please enter a search term');
         return;
